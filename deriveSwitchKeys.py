ec6eb2b397ebe3a44e9ee33085c5829f4dbc235e
socraticbliss <socraticbliss@users.noreply.github.com>
2018-04-06 17:21:00 -0500

    Update and rename deriveSwitchKeys(PK21andTitle).py to deriveSwitchKeys.py

--- deriveSwitchKeys.py
+++ deriveSwitchKeys.py
@@ -1,10 +1,9 @@
 # Derive Switch Keys [PK21, Title]
-# SocraticBliss (Mastarifla)
+# SocraticBliss [Mastarifla]
 # Copyright 2018 All rights reserved
 # Feel free to submit code changes/optimizations/updates
 
 from binascii import unhexlify as uhx, hexlify as hx
-from Crypto.Cipher import AES
 import sys
 
 master_keys = ['XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
@@ -19,17 +18,20 @@ def decrypt(keyName, sourceKey):
 	for key in range(len(master_keys)):
 		# Create our AES MasterKey
 		aes_masterKey = AES.new(uhx(master_keys[key]), AES.MODE_ECB)
-		# Use our AES MasterKey to Decrypt our Package2/TitleKek Source Key
+		# Use our AES MasterKey to Decrypt our Source Key
 		dec_sourceKey = aes_masterKey.decrypt(uhx(sourceKey))
-		# Print out the Generated Package2/TitleKek Keys [MasterKey Enumerated]
+		# Print out the Generated Keys [MasterKey Enumerated]
 		print('%s%d = %s' % (keyName, key, hx(dec_sourceKey).upper()))
 
 def main():
 	try:
+		from Crypto.Cipher import AES
 		decrypt('package2_key_0', package2_key_source)
 		decrypt('titlekek_0', titlekek_source)
 	except TypeError:
 		print('You forgot to add your prerequisite keys to the script...')
+	except ImportError:
+		print('You forgot to install pycrypto...')
 	
 if __name__ == '__main__':
 	sys.exit(main())
